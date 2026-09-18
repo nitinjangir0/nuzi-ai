0966f9619eb8c8219e05d5e0ecb1c499c58e938a
nitinjangir0 <jangirnitin217@gmail.com>
2026-09-18 23:50:18 +0530

    Update backend configuration

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(process.env.MONGO_URl);
+        const connection = await mongoose.connect(process.env.MONGO_URI);
 
         console.log(`MongoDB connected: ${connection.connection.host}`);
     } catch (error) {
